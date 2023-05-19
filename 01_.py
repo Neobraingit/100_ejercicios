244e2b4ed99e9720fcaf4636896655d73a5a43cf
Neobraingit <Maervca2@icloud.com>
2023-05-19 19:24:23 +0200

    100 ejercicios

---
+++ 01_.py
@@ -0,0 +1,7 @@
+'''Escribir una secuencia de instrucciones de Python  que permita declarar tres variables a, b y c asignçandoles a cada una una valor
+1, Francia y 3.62 y mostrar esos valores por consola'''
+
+print ('Declara tres variables en una sola línea: ')
+entero, pais, decimal = 1, 'Francia', 3.62
+
+print (entero, pais, decimal)
---
+++ 02_.py
@@ -0,0 +1,6 @@
+''' Escribir una secuencia de instrucciones que permita declarar una varibale ch iniciándola con el valor "hola"
+y luego modificarla para que contenga "está bien". Mostrar el contenido de la variable por consola'''
+
+ch = 'hola '
+ch = ch + 'está bien'
+print (ch)
