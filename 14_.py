0984b079140689f008fc19b1830c8fe2e13489fc
Neobraingit <Maervca2@icloud.com>
2023-05-19 20:28:54 +0200

    100 ejercicios

---
+++ 14_.py
@@ -0,0 +1,8 @@
+# Crear una lista vacía y llenarla  con estos valores: (10, 25, 30, 45, 90) y las strings:(ab, cd, ef.
+
+lista = []
+valores = (10, 25, 30, 45, 90, 'ab', 'cd', 'ef')
+for i in valores:
+    lista.append(i)
+    
+print (lista)
