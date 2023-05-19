07966d0141541d1bd48ffcead766828ca2086497
Neobraingit <Maervca2@icloud.com>
2023-05-19 20:23:50 +0200

    100 ejercicios

---
+++ 13_.py
@@ -0,0 +1,8 @@
+'''Crear una lista  con los siguientes valores (3, 2, 2, 1, 9, 1, 2, 3, 7) y mostrar el número de ocurrencias del número 1'''
+
+ocurrencias = [3, 2, 2, 1, 9, 1, 2, 3, 7]
+
+for i in ocurrencias:
+    print ('El número 1 aparece: ',ocurrencias.count(1), 'veces')
+    break
+    
