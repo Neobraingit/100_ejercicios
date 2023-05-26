33a8026135eaf8de71adcbda82f87c1bce88ad9b
Neobraingit <Maervca2@icloud.com>
2023-05-26 17:06:05 +0200

    100 ejercicios

---
+++ 72_.py
@@ -0,0 +1,8 @@
+'''Escribir un programa que tenga una frase y una palabra y se debe
+reemplazar una palabra de la frase con dicha palabra'''
+
+frase = input('Introduce una frase: ')
+palabra = input ('Introduce una palabra: ')
+
+frase.replace('Esto', palabra)
+print (frase)
