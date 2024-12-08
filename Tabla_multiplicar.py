90e79b85bdaaffc9896fa8036ae3a68ee089267f
neobraingit <neobrain@protonmail.com>
2024-12-08 16:11:49 +0100

    Tabla_multiplicar_inversa

---
+++ Tabla_multiplicar.py
@@ -0,0 +1,12 @@
+
+import argparse
+from termcolor import colored
+
+parser = argparse.ArgumentParser(description='Tabla de multiplicar')
+parser.add_argument('-n', '--numero', type=int, required=True,help='Dime un número')
+
+args = parser.parse_args()
+
+for i in range (11, 0, -1):
+   resultado =  i * args.numero
+   print (colored(f'{args.numero} x {i} = {resultado}', 'red'))
