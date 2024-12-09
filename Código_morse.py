12e14f7950bfd9f7be70c28b80226e0e2e124a3c
neobraingit <neobrain@protonmail.com>
2024-12-09 07:49:02 +0100

    Código_morse

---
+++ Código_morse.py
@@ -0,0 +1,10 @@
+
+código = {'a' : '.-', 'b' : '-..'}
+
+frase = input('Introduce el mensaje a traducir: ')
+
+if 'a' in frase:
+    print (código['a'])
+elif 'b' in frase:
+    print (código['b'])
+    
