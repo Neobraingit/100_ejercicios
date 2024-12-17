239e4dbcc0d98f395947b8cc7135fa42f0633ebf
neobraingit <neobrain@protonmail.com>
2024-12-17 20:09:44 +0100

    Dados

---
+++ Simulador_dados.py
@@ -0,0 +1,12 @@
+
+
+import random
+
+numero1 = random.randint(1, 6)
+numero2 = random.randint(1, 6)
+
+print (f'La tirada es de {numero1}')
+print (f'La tirada dos es de {numero2}')
+
+suma = numero1 + numero2
+print (f'El resultado de las tiradas es de {suma}')
